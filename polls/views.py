421ebd4d00dfffc93afe4099e8fc577948ca8928
MitkumarR <mitkumar1977@gmail.com>
2024-12-19 15:28:21 +0530

    template index.html is added

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,22 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from pywin.tools.browser import template
+
+from polls.models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def details(request, question_id):
     return HttpResponse("You are looking at question %s." % question_id)

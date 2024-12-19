ef9b7a6a470e9d7fc41afe9e11bd1b99bd0b2c9a
MitkumarR <mitkumar1977@gmail.com>
2024-12-19 18:01:33 +0530

    used some shortcuts from django libraries and added urls updates and namespacing

--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,41 @@
-from lib2to3.fixes.fix_input import context
+# from lib2to3.fixes.fix_input import context
 
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
 from django.http import HttpResponse
 from django.template import loader
 from pywin.tools.browser import template
 
 from polls.models import Question
 
+#load template -> fill context -> return HttpResponse object
+# dex(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# short cut in django
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
-def details(request, question_id):
-    return HttpResponse("You are looking at question %s." % question_id)
+def detail(request, question_id):
+
+    question = get_object_or_404(Question,pk=question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    return render(request, "polls/detail.html", {"question": question} )
 
 def results(request, question_id):
     response = "You are looking at the result of question %s."
